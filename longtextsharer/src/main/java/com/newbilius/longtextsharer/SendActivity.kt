cbd01ad2ff00980ca113c8e784c6233b30beeeb5
Oleg Dantsaranov <oleg.dants@icloud.com>
2015-07-29 17:22:29 +0300

    add anko for neat creating ui in code

--- longtextsharer/src/main/java/com/newbilius/longtextsharer/SendActivity.kt
+++ longtextsharer/src/main/java/com/newbilius/longtextsharer/SendActivity.kt
@@ -1,6 +1,8 @@
 package com.newbilius.longtextsharer
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.*
@@ -20,6 +22,7 @@ import android.view.View
 import android.widget.ImageView
 import com.newbilius.longtextsharer.R
 import kotlinx.android.synthetic.activity_send.*
+import org.jetbrains.anko.*
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
@@ -36,16 +39,11 @@ public class SendActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.getItemId()) {
             R.id.menu_action_about -> {
-                val builder = AlertDialog.Builder(this);
-                with(builder) {
-                    setMessage(R.string.about_alert_text)
-                    setTitle(R.string.about_alert_title)
-                    setPositiveButton(R.string.about_alert_other_progs, { dialogInterface, button ->
-                        val browseIntent2 = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/developer?id=%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9+%D0%9C%D0%BE%D0%B8%D1%81%D0%B5%D0%B5%D0%B2"))
-                        startActivity(browseIntent2)
-                    })
-                }
-                builder.create().show();
+                alert(R.string.about_alert_text, R.string.about_alert_title) {
+                    positiveButton(R.string.about_alert_other_progs) {
+                        browse("https://play.google.com/store/apps/developer?id=%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9+%D0%9C%D0%BE%D0%B8%D1%81%D0%B5%D0%B5%D0%B2")
+                    }
+                }.show()
             }
         }
         return true;
@@ -74,26 +72,17 @@ public class SendActivity : AppCompatActivity() {
                 bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream);
                 stream.close();
 
-                val builder = AlertDialog.Builder(this);
                 val imageView = ImageView(this);
 
                 imageView.setImageBitmap(bitmap);
 
-                with(builder) {
-                    setView(imageView)
-                    setTitle(R.string.preview_alert_title)
-                    setPositiveButton(R.string.preview_alert_ok, { dialogInterface, button ->
-                        val shareIntent = Intent();
-                        shareIntent.setAction(Intent.ACTION_SEND);
-                        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(File(fileName)));
-                        shareIntent.setType("image/png");
-                        startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.app_name)));
-                        finish();
-                    })
-                    setNegativeButton(R.string.preview_alert_cancel, { dialogInterface, button -> })
-                }
-
-                builder.create().show();
+                alert(R.string.preview_alert_title) {
+                    customView(imageView)
+                    positiveButton(R.string.preview_alert_ok) {
+                        share(R.string.app_name, "image/png", fileName)
+                    }
+                    negativeButton(R.string.preview_alert_cancel) { dismiss() }
+                }.show()
 
             } catch (e: FileNotFoundException) {
                 alertError(R.string.error_iofile);
@@ -105,17 +94,24 @@ public class SendActivity : AppCompatActivity() {
         };
     }
 
-    fun alertError(text: String) {
-        val builder = AlertDialog.Builder(this);
-        with(builder) {
-            setTitle(R.string.error_title)
-            setMessage(text)
-            setPositiveButton(R.string.ok, { dialogInterface, button -> })
-        }
-        builder.create().show();
+    fun alertError(resourceId: Int) {
+        alert(resourceId, R.string.error_title) {
+            positiveButton(R.string.ok) { dismiss() }
+        }.show()
     }
 
-    fun alertError(resourceId: Int) {
-        alertError(getString(resourceId))
+    // Inspired by Context.share() from anko ContextUtils.kt
+    // Probably should make it more generic and propose for including in anko
+    fun share(title: Int, type: String, file: String): Boolean {
+        try {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setType(type)
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(File(file)))
+            startActivity(Intent.createChooser(intent, getText(title)))
+            return true
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+            return false
+        }
     }
 }
